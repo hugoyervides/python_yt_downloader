f03742f269dacef1146aed29891c11fa8ffa93e8
Victor Oyervides <victor.covarrubias@hotmail.com>
2020-03-29 02:52:03 -0600

    add routes to flask

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify, Response, send_file
+from video_manager import download_video, exists, get_file_path
+from bson import json_util
 app = Flask(__name__)
 
-@app.route('/data/<video>')
+@app.route('/convert/<video>')
 def get_data(video):
-    #TODO - Check if the video exists and if not download it and send the data of it
+    #Check if we have the video
+    data = exists(video)
+    if 'error' in data:
+        return Response(
+                json_util.dumps(download_video(video)),
+                mimetype='application/json')
+    return Response(
+            json_util.dumps(data),
+            mimetype='application/json')
 
 @app.route('/stream/<video>')
 def get_stream(video):
-    #TODO - Check if the video exists and if not download it and return the stream of the MP3
+    #Check if the file exists
+    data = exists(video)
+    if 'error' in data:
+        return Response(
+                json_util.dumps(data),
+                mimetype='application/json')
+    #Stream the song
+    return send_file(get_file_path(video), mimetype='audio/mpeg')
 
 @app.route('/download/<video>')
-def download_video(video):
-    #TODO- Check if we have the video and if not download it and return a download of the MP3
+def download(video):
+    #Check if the file exists
+    data = exists(video)
+    if 'error' in data:
+        return Response(
+                json_util.dumps(data),
+                mimetype='application/json')
+    #Stream the song
+    return send_file(get_file_path(video),as_attachment=True ,mimetype='audio/mpeg')
 
 @app.route('/exists/<video>')
 def check_video(video):
-    #TODO- Check if the video exists on the database and if it exists
+    return Response(
+            json_util.dumps(exists(video)),
+            mimetype='application/json')
 
 #DEV APP RUN 
 if __name__ == "__main__":
--- video_manager.py
+++ video_manager.py
@@ -1,9 +1,14 @@
 import pymongo
 from pytube import YouTube
 from pydub import AudioSegment
+import time
 import os
 
+#CONFIG 
 DOWNLOAD_PATH = './downloads'
+APP_URL = 'http://localhost:5000/'
+MAX_CACHE_SIZE = 8000 #This is in MB
+PURGE_SIZE = 100 #Amount of data to delete if we reach the MAX_CACHE_SIZE limit
 
 #Connect to the Database
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -12,27 +17,85 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["pythonyt"]
 mycol = mydb["videos"]
 
+def get_file_path(videoId):
+    return DOWNLOAD_PATH + '/' + videoId + '.mp3'
+
 #Function to check if a video exists in the database
 def exists(videoId):
     query = { 'videoId' : videoId }
-    if mycol.find(query).count() > 0:
-        return True
+    result = mycol.find(query)
+    if result.count() > 0:
+        return result[0]
     else:
-        return False
+        return {'error' : 'File does not exist on the server'}
+
+#function to purge unused videos
+def purge_cache():
+    print('[VIDEO MANAGER] Storage is full! deleting unused songs')
+    #get most unused songs
+    songs = mycol.find().sort('downloadTimestamp')
+    dataDeleted = 0
+    numberOfFilesDeleted = 0
+    for s in songs:
+        if dataDeleted < PURGE_SIZE:
+            filePath = DOWNLOAD_PATH + '/' + s['videoId'] + '.mp3'
+            dataDeleted += (1 * pow(10,-6)) * os.path.getsize(filePath)
+            numberOfFilesDeleted += 1
+            #Delete from db and system
+            query = {'videoId':s['videoId']}
+            mycol.delete_one(query)
+            os.remove(filePath)
+    print('[VIDEO MANAGER] Purge completed ' + str(numberOfFilesDeleted) + ' files deleted, ' + str(dataDeleted) + 'MB of data deleted')
+
+def space_used():
+    totalSize = 0
+    for filename in os.listdir(DOWNLOAD_PATH):
+        path = DOWNLOAD_PATH + '/' + filename
+        totalSize += (1 * pow(10,-6)) * os.path.getsize(path)
+    return totalSize
 
 #Function to download a new video
 def download_video(videoId):
-    videoURL = 'https://youtube.com/watch?v=' + videoId
-    yt = YouTube(videoURL)
-    #get the stream and download
-    stream = yt.streams.filter(only_audio=True).first()
-    filePath = stream.download(DOWNLOAD_PATH,videoId)
-    mp4_audio = AudioSegment.from_file(filePath, "mp4")
-    #Export it to mp3
-    mp4_audio.export(DOWNLOAD_PATH + '/' + videoId + '.mp3', format='mp3')
-    #Remove the old mp4 file
-    os.remove(filePath)
-
+    #check if we have not reached the 
+    if space_used() > MAX_CACHE_SIZE:
+        purge_cache()
+    #Check if the video already exists in the db
+    if 'error' in exists(videoId):
+        print('[VIDEO MANAGER] Starting download of video ' + videoId)
+        videoURL = 'https://youtube.com/watch?v=' + videoId
+        try:
+            yt = YouTube(videoURL)
+        except:
+            return { 'error' : 'Error finding video'}
+        #check if video is not over 10 mintues
+        if yt.length > 630:
+            print('[VIDEO MANAGER] Error, video is too long!!')
+            return { 'error': 'Video is to long!'}
+        videoInfo = { "videoId": videoId, 
+                    "title" : yt.title, 
+                    "length" : yt.length, 
+                    "author" : yt.author, 
+                    "description": yt.description, 
+                    "streamURL" : APP_URL + 'stream/' + videoId,
+                    "downloadURL" : APP_URL + 'download/' + videoId,
+                    "downloadTimestamp": int(time.time())}
+        #get the stream and download
+        stream = yt.streams.filter(only_audio=True).first()
+        filePath = stream.download(DOWNLOAD_PATH,videoId)
+        print('[VIDEO MANAGER] Video downloaded, converting file to MP3')
+        mp4_audio = AudioSegment.from_file(filePath, "mp4")
+        #Export it to mp3
+        metadata = {'artist': videoInfo['author'], 'album': videoInfo['title']}
+        mp4_audio.export(DOWNLOAD_PATH + '/' + videoId + '.mp3', format='mp3', tags=metadata)
+        print('[VIDEO MANAGER] Convertion complete, inserting video on DB and deleting mp4 file')
+        #Remove the old mp4 file
+        os.remove(filePath)
+        #Add the song to the database
+        mycol.insert_one(videoInfo)
+        print('[VIDEO MANAGER] Success!')
+        return videoInfo
+    else:
+        print('[VIDEO MANAGER] Video ' + videoId + ' already exists on the server, skiping')
+        query = { 'videoId' : videoId }
+        return mycol.find_one(query)
     
-
-download_video('e-IWRmpefzE')
