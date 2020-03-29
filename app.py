0148856d61a702caf2cf2ed2f5093807d83bce24
Victor Oyervides <victor.covarrubias@hotmail.com>
2020-03-29 03:04:10 -0600

    Remove extra code

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ from video_manager import download_video, exists, get_file_path
 from bson import json_util
 app = Flask(__name__)
 
+def get_file(video, attachment):
+    #Check if the file exists
+    data = exists(video)
+    if 'error' in data:
+        return Response(
+                json_util.dumps(data),
+                mimetype='application/json')
+    #Stream the song
+    return send_file(get_file_path(video),as_attachment=attachment ,mimetype='audio/mpeg')
+
 @app.route('/convert/<video>')
 def get_data(video):
     #Check if we have the video
@@ -17,25 +27,11 @@ def get_data(video):
 
 @app.route('/stream/<video>')
 def get_stream(video):
-    #Check if the file exists
-    data = exists(video)
-    if 'error' in data:
-        return Response(
-                json_util.dumps(data),
-                mimetype='application/json')
-    #Stream the song
-    return send_file(get_file_path(video), mimetype='audio/mpeg')
+    return get_file(video, False)
 
 @app.route('/download/<video>')
 def download(video):
-    #Check if the file exists
-    data = exists(video)
-    if 'error' in data:
-        return Response(
-                json_util.dumps(data),
-                mimetype='application/json')
-    #Stream the song
-    return send_file(get_file_path(video),as_attachment=True ,mimetype='audio/mpeg')
+    return get_file(video, True)
 
 @app.route('/exists/<video>')
 def check_video(video):
